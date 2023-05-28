460012cf2d75c6c0c7502aabdfb884d8b84cafe9
MattiaQuartana <46351490+MattiaQuartana@users.noreply.github.com>
2023-05-28 15:03:24 +0300

    Migration Computer file to git
    Migration Computer file to git

---
+++ src/script.js
@@ -0,0 +1,183 @@
+// Contenuto di script.js
+
+
+
+// ------------------------------------------------- Page Curriculum
+document.addEventListener('DOMContentLoaded', function () {
+    // Funzione per gestire il click su #1980
+    function handleClick() {
+        // Ottenere l'elemento #picturestime1980
+        var picturestime1980 = document.getElementsByClassName('picturestime1980');
+        
+        // Verificare se le immagini di picturestime1980 sono visibili
+        var isVisible = picturestime1980[0].style.display !== 'none';
+        
+        // Invertire la visibilità delle immagini di picturestime1980
+        for (var i = 0; i < picturestime1980.length; i++) {
+            picturestime1980[i].style.display = isVisible ? 'none' : 'block';
+        }
+       
+        
+        // Cambiare il colore del font di #1980 !!!
+        this.style.color = isVisible ? 'white' : '#D83431';
+    }
+
+    // Nascondere le immagini di picturestime1980 all'avvio
+    var picturestime1980 = document.getElementsByClassName('picturestime1980');
+    for (var i = 0; i < picturestime1980.length; i++) {
+        picturestime1980[i].style.display = 'none';
+    }
+    
+    
+    // Ottenere l'elemento #1980
+    var element1980 = document.getElementById('year-1980');
+    
+    
+    // Aggiungi un gestore di eventi al click su #1980
+    element1980.addEventListener('click', handleClick);
+    
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Funzione per gestire il click su #1980
+    function handleClick() {
+        // Ottenere l'elemento #picturestime1980
+        
+        var picturestime1985 = document.getElementsByClassName('picturestime1985');
+        
+        
+        // Verificare se le immagini di picturestime1980 sono visibili
+        
+        var isVisible = picturestime1985[0].style.display !== 'none';
+       
+
+        // Invertire la visibilità delle immagini di picturestime1980
+        
+        for (var i = 0; i < picturestime1985.length; i++) {
+            picturestime1985[i].style.display = isVisible ? 'none' : 'block';
+        }
+        
+        // Cambiare il colore del font di #1980 !!!
+        this.style.color = isVisible ? 'white' : '#D83431';
+    }
+
+    // Nascondere le immagini di picturestime1980 all'avvio
+    
+    var picturestime1985 = document.getElementsByClassName('picturestime1985');
+    for (var i = 0; i < picturestime1985.length; i++) {
+        picturestime1985[i].style.display = 'none';
+    }
+    
+    // Ottenere l'elemento #1980
+    
+    var element1985 = document.getElementById('year-1985');
+   
+    // Aggiungi un gestore di eventi al click su #1980
+   
+    element1985.addEventListener('click', handleClick);
+    
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Funzione per gestire il click su #1980
+    function handleClick() {
+        // Ottenere l'elemento #picturestime1980
+        var picturestime1990 = document.getElementsByClassName('picturestime1990');
+        
+        // Verificare se le immagini di picturestime1980 sono visibili
+        var isVisible = picturestime1990[0].style.display !== 'none';
+       
+        // Invertire la visibilità delle immagini di picturestime1980
+       
+        for (var i = 0; i < picturestime1990.length; i++) {
+            picturestime1990[i].style.display = isVisible ? 'none' : 'block';
+        }
+        
+        // Cambiare il colore del font di #1980 !!!
+        this.style.color = isVisible ? 'white' : '#D83431';
+    }
+
+    // Nascondere le immagini di picturestime1980 all'avvio
+    
+    var picturestime1990 = document.getElementsByClassName('picturestime1990');
+    for (var i = 0; i < picturestime1990.length; i++) {
+        picturestime1990[i].style.display = 'none';
+    }
+    
+    // Ottenere l'elemento #1980
+    var element1990 = document.getElementById('year-1990');
+   
+    // Aggiungi un gestore di eventi al click su #1980
+    element1990.addEventListener('click', handleClick);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Funzione per gestire il click su #1980
+    function handleClick() {
+        // Ottenere l'elemento #picturestime1980
+        var picturestime1997 = document.getElementsByClassName('picturestime1997');
+        
+        // Verificare se le immagini di picturestime1980 sono visibili
+        var isVisible = picturestime1997[0].style.display !== 'none';
+       
+
+        // Invertire la visibilità delle immagini di picturestime1980
+       
+        for (var i = 0; i < picturestime1997.length; i++) {
+            picturestime1997[i].style.display = isVisible ? 'none' : 'block';
+        }
+       
+        // Cambiare il colore del font di #1980 !!!
+        this.style.color = isVisible ? 'white' : '#D83431';
+    }
+
+    // Nascondere le immagini di picturestime1980 all'avvio
+    
+    var picturestime1997 = document.getElementsByClassName('picturestime1997');
+    for (var i = 0; i < picturestime1997.length; i++) {
+        picturestime1997[i].style.display = 'none';
+    }
+   
+    // Ottenere l'elemento #1980
+    
+    var element1997 = document.getElementById('year-1997');
+
+    // Aggiungi un gestore di eventi al click su #1980
+    
+    element1997.addEventListener('click', handleClick);
+   
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Funzione per gestire il click su #1980
+    function handleClick() {
+        // Ottenere l'elemento #picturestime1980
+        var picturestime1999 = document.getElementsByClassName('picturestime1999');
+        
+        // Verificare se le immagini di picturestime1980 sono visibili
+
+        var isVisible = picturestime1999[0].style.display !== 'none';
+
+        // Invertire la visibilità delle immagini di picturestime1980
+        
+        for (var i = 0; i < picturestime1999.length; i++) {
+            picturestime1999[i].style.display = isVisible ? 'none' : 'block';
+        }
+        // Cambiare il colore del font di #1980 !!!
+        this.style.color = isVisible ? 'white' : '#D83431';
+    }
+
+    // Nascondere le immagini di picturestime1980 all'avvio
+    
+    var picturestime1999 = document.getElementsByClassName('picturestime1999');
+    for (var i = 0; i < picturestime1999.length; i++) {
+        picturestime1999[i].style.display = 'none';
+    }
+    // Ottenere l'elemento #1980
+    
+    var element1999 = document.getElementById('year-1999');
+    // Aggiungi un gestore di eventi al click su #1980
+    
+    element1999.addEventListener('click', handleClick);
+});
+// ------------------------------------------------- END Page Curriculum
